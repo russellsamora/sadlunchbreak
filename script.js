842da08a2b32ded8501a2c9a72f3deb66afd791d
russellgoldenberg <russellgoldenberg@gmail.com>
2018-10-23 17:38:31 -0400

    db

---
+++ script.js
@@ -0,0 +1,43 @@
+(function() {
+  var ul = d3.select('ul.links');
+  var url = 'https://pudding.cool/misc/sadlunchbreak/data.json';
+  var media = {
+    text: '📖',
+    video: '📺',
+    interactive: '🕹️',
+    image: '🖼️',
+    other: '⁉️'
+  };
+
+  function getIcon(d) {
+    var m = d['Media Type'];
+    return m ? media[m] : media.other;
+  }
+
+  d3.json(url)
+    .then(function(response) {
+      console.log('last updated: ', new Date(response.timestamp));
+      var li = ul
+        .selectAll('.li')
+        .data(response.data)
+        .enter()
+        .append('li');
+
+      li.append('span').text(getIcon);
+
+      li.append('a')
+        .attr('href', function(d) {
+          return d.Link;
+        })
+        .attr('target', '_blank')
+        .text(function(d) {
+          return d.Title;
+        });
+
+      li.append('time').text(function(d) {
+        var date = new Date(d.Timestamp);
+        return '' + d3.timeFormat('%b %d')(date) + '';
+      });
+    })
+    .catch(console.error);
+})();
